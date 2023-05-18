f7ec7bae64770f5fc78668d85fa94d37ca73d5b6
Umbeee <umberto.nesta@hotmail.com>
2023-05-18 14:08:52 +0200

    creazione repo

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-badwords</title>
+</head>
+<body> 
+    <div class="container">
+        <form action="">
+    
+            <input type="text" placeholder="inserisci paragrafo">
+            <input type="text" placeholder="inserisci parola da censurare">
+
+            <button type="submit">invia</button>
+    
+        </form>
+    </div>
+</body>
+</html>
